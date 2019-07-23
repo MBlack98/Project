9a6f8ed32a844bfb2f08d11981858f1c2548a272
MBlack98 <m.schwarz@student.unsw.edu.au>
2019-07-23 13:19:12 +1000

    Adding new files

---
+++ functions.R
@@ -0,0 +1,45 @@
+####
+##  Functions used in Shark Distribution.Rmd
+##  
+##  author: Max Schwarz
+##  date: 12th July 2019
+####
+
+import_data <- function(species_name) {
+  #
+  # Import data from GBIF for a species
+  #
+  # Argument:
+  # ---
+  # species_name: Species name provided
+  #
+  species1 <- name_suggest(q = species_name) # find the 'key' of the species
+  
+  # using the key, find all data with the occ_search function
+  
+  species.occ <- occ_search(taxonKey = species1[1,1], limit = 21000, return = 'data', 
+                            hasCoordinate = TRUE)
+  
+  # use the species.occ object and change some column names (mutate) 
+  # and then create a tibble based on selected columns
+  
+  species.occ %>%
+    mutate(species_name = species1$canonicalName[1], key = taxonKey) %>%
+    dplyr::select(species_name, key, latitude = decimalLatitude, longitude = decimalLongitude) %>%
+    tibble()
+}
+
+create_map <- function(species_name) {
+  # 
+  # Create a map for each species
+  #
+  # Argument:
+  # ---
+  # species_name: Species name provided
+  #
+  leaflet(data = distributions[[species_name]][["."]]) %>%
+    addTiles() %>%
+    addCircleMarkers(lat = ~latitude, lng = ~longitude, radius = 6, stroke = FALSE, 
+                     color = "orange", fillOpacity = 0.5)
+  }
+  
